e813207d57e940669b33270e38937219be98c424
Mayank Raj Parmar <mayankrajparmar@gmail.com>
2025-01-07 11:40:14 +0530

    Find All Numbers Disappeared in an Array

---
+++ findAllNumbersDisappearedinArray.js
@@ -0,0 +1,29 @@
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+// Example 1:
+
+const nums = [4, 3, 2, 7, 8, 2, 3, 1];
+// Output: [5,6]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [2]
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 105
+// 1 <= nums[i] <= n
+
+var findDisappearedNumbers = function (nums) {
+  const uniqueValues = new Set(nums);
+  const arr = [];
+  for (let i = 1; i <= nums.length; i++) {
+    if (!uniqueValues.has(i)) {
+      arr.push(i);
+    }
+  }
+  return arr;
+};
+
+console.log(findDisappearedNumbers(nums));
